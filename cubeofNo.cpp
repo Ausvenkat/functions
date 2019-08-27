6faf4ad072de82351e2233489ca775a7fee40d2d
Ausvenkat <36623888+Ausvenkat@users.noreply.github.com>
2019-08-27 15:41:07 +0530

    Create cubeofNo.cpp

---
+++ cubeofNo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+double cube(double num);
+int main()
+{
+    int num;
+    double c;
+    cin>>num;
+    c = cube(num);
+    cout<<c;
+    
+    return 0;
+}
+double cube(double num)
+{
+    return (num * num * num);
+}
